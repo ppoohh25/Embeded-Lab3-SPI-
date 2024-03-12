4f82c30ba8f270a1ca3f064473ee2da7294b3b24
Sadayu Dachdachasunan <93990764+ppoohh25@users.noreply.github.com>
2024-03-12 11:30:59 +0700

    Add files via upload

---
+++ UART.ino
@@ -0,0 +1,26 @@
+
+void serial_putc(char data) {
+  char busy;
+  do { busy = UCSR0A & 0b00100000; } while (busy == 0);
+  UDR0 = data;
+}
+
+void serial_puts(char *data) {
+  while (*data) {
+    serial_putc(*data++);
+  }
+}
+
+char serial_getc(){
+  char busy;
+  do{ busy = UCSR0A & 0b10000000;}
+  while(busy == 0);
+  return(UDR0);
+}
+
+ISR(UART_RX_vect) {
+  t[z] = UDR0;
+  serial_putc(t[z]);
+  serial_puts("\n");
+  z += 1;
+}
